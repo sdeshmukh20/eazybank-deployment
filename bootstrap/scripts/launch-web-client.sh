3d800849d6e439249ac4978649b50ee96e9eed17
Satyajeet Deshmukh <sdeshmukh@mobiquityinc.com>
2024-12-06 19:32:45 +0530

    release-11: eazybank project setup

--- bootstrap/scripts/launch-web-client.sh
+++ bootstrap/scripts/launch-web-client.sh
@@ -17,7 +17,15 @@ fi
 
 ENV_NAME=$1;
 
-cd ../../../client-ui/eazy-bank-ui || { echo "Failed to enter eazy-bank-ui directory"; exit 1; }
+cd ../../../../client-ui/eazy-bank-ui || { echo "Failed to enter eazy-bank-ui directory from current-directory:$(pwd)"; exit 1; }
 
 npm install -g @angular/cli
-ng serve --configuration="${ENV_NAME}"
+npm install
+# Run ng serve in the background and detach it
+ng serve --configuration="${ENV_NAME}" > ng-serve.log 2>&1 &
+
+# Get the PID of the background process
+NG_SERVE_PID=$!
+
+echo "ng serve is running in the background with PID: $NG_SERVE_PID"
+echo "Logs are being written to ng-serve.log"
